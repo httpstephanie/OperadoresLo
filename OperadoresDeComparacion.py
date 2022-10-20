d015575adb6b87a857b690a735f16cb8a69b7964
httpstephanie <110868588+httpstephanie@users.noreply.github.com>
2022-10-20 11:42:47 -0500

    Add files via upload

---
+++ OperadoresDeComparacion.py
@@ -0,0 +1,41 @@
+#mi_bool = 10 == 25
+#== igual a
+#!= diferente a
+#print(mi_bool)
+#-------------------------------------------------------
+#Práctica Operadores de Comparación 1
+"""Crea dos variables (num1 y  num2) con los valores 36 y 17 respectivamente. Verifica si num1 es mayor o igual que num2 y almacena el resultado de dicha comparación en una variable llamada mi_bool"""
+#num1 =36
+#num2 =17
+#mi_bool = num1 >= num2
+#print(mi_bool)
+#-------------------------------------------------------
+#Práctica Operadores de Comparación 2
+"""Crea dos variables (num1 y  num2):
+Dentro de num1, almacena el resultado de la operación raíz cuadrada de 25
+Dentro de num2, almacena el número 5.
+Verifica si num1 es igual a num2 y almacena el resultado de dicha comparación en una variable llamada mi_bool."""
+#num1 = 25**0.5
+#num2 = 5
+#mi_bool = num1 == num2
+#-------------------------------------------------------
+#Práctica Operadores de Comparación 3
+"""Crea dos variables (num1 y  num2):
+Dentro de num1, almacena el resultado de la operación 64 x 3
+Dentro de num2, almacena el resultado de la operación 24 x 8
+Verifica si num1 es diferente a num2 y almacena el resultado de dicha comparación en una variable llamada mi_bool."""
+#num1 = 64*3
+#num2 = 24*8
+#mi_bool = num1 != num2
+#-------------------------------------------------------
+#Operadores logicos
+#and es Y
+#or es O
+#mi_bool = (4 < 5) and (5 == 2+3) 
+#print(mi_bool)
+#-------------------------------------------------------
+#texto = "Esta frase es breve"
+#mi_bool = ('frase' in texto) and ('breve' in texto)
+#------------------------------------------------------
+#mi_bool = not 'a' == 'a'
+# print(mi_bool)
